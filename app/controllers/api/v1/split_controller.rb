df8a50c96a22af8367c739961bace4e321e9f576
ayushgoyal162 <ayush.goyal@intellolabs.com>
2022-12-18 11:56:09 +0530

    changes in api

--- app/controllers/api/v1/split_controller.rb
+++ app/controllers/api/v1/split_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::SplitController < ApplicationController
   def index
-    users = User.all
+    users = User.all.pluck(:id,:name)
     render json: users, status: 200
   end
 
@@ -10,20 +10,27 @@ class Api::V1::SplitController < ApplicationController
 
   def get_groups
     groups = Group.all
-    render json: groups, status: 200
+    arr=[]
+    groups.each do |group|
+      data={}
+      data["name"] = group.name.to_s rescue ""
+      data["members"] = group.users.pluck(:id,:name)
+      arr << data
+    end
+    render json: arr, status: 200
   end
 
   def make_group
     group = Group.new(name: params[:name])
     if group.save
-      users_list = params[:users]
+      users_list = JSON.parse(params[:users])
       users_name_list = []
       users_list.each do |user_id|
         new_entry = UserGroup.new(group_id: group.id, user_id: user_id)
         users_name_list << User.find(user_id).name
         new_entry.save
       end
-      render json:{group_id => group.id, members_added => users_name_list }, status: 200
+      render json:{"group_id" => group.id, "members_added" => users_name_list }, status: 200
     end
   end
 
@@ -45,14 +52,22 @@ class Api::V1::SplitController < ApplicationController
 
   def get_expenses
     users = UserDebt.where(person_id: params[:user_id])
+    check = false
+    if users.nil? or users.length == 0
+      puts users = UserDebt.where(person_with_id: params[:user_id])
+      check = true
+
+    end
     js = []
     users.each do |a|
       hash = {}
       milneKa = a.debt.to_i rescue 0
-      check_debt = UserDebt.find_by(person_id: a.person_with_id, person_with_id: params[:user_id]) 
+      puts check_debt = UserDebt.find_by(person_id: a.person_with_id, person_with_id: params[:user_id]) if check == false
+      puts check_debt = UserDebt.find_by(person_id: a.person_id, person_with_id: params[:user_id]) if check == true
       actualDebt = check_debt.debt.to_i rescue 0
       personName = User.find(params[:user_id]).name rescue "NIL"
-      fromName = User.find(a.person_with_id).name rescue "NIL"
+      fromName = User.find(a.person_with_id).name rescue "NIL" if check == false
+      fromName = User.find(a.person_id).name rescue "NIL" if check == true
       final = milneKa - actualDebt rescue 0
       if final < 0
         msg = fromName + " ko dene ha "
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       get "/get_groups", to: "split#get_groups"
       post "/split_payment", to: "split#split_payment"
       post "/get_expenses", to: "split#get_expenses"
+      post "/make_group", to: "split#make_group"
       get "/get_transactions", to: "split#get_transactions"
       resources :split, only: [:index, :show, :create, :update, :create_group]
     end
